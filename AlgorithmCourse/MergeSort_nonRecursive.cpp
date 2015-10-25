f5e639ab1151b9746c4a21744e574b555f003d0d
Rosen <rosen9212@gmail.com>
2015-10-25 19:08:51 +0800

    algorithm course homework code

---
+++ AlgorithmCourse/MergeSort_nonRecursive.cpp
@@ -0,0 +1,82 @@
+//非递归归并
+//无回移，合并最长有序序列
+#include <cstdio>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+void printArray(int arr[],int size_arr){
+  for(int i=0;i<size_arr;i++)cout<<arr[i]<<" ";
+  cout<<endl;
+}
+
+void move(int from[],int l,int r,int to[]){
+  for(int i=l;i<r;i++)to[i]=from[i];
+}
+
+void mergeArray(int from[],int l,int m,int r,int to[]){
+  int k=l,i,j;
+  for(i=l,j=m;i<m&&j<r;k++){
+    if(from[i]<from[j])to[k]=from[i++];
+    else to[k]=from[j++];
+  }
+  if(i>=m)while(j<r)to[k++]=from[j++];
+  if(j>=r)while(i<m)to[k++]=from[i++];
+}
+
+void mergeSort(int arr[],int n){
+  //temp为临时中转数组，flag标明当前有序数组存储在哪个数组中
+  int *temp = new int[n];
+  bool flag=true;
+  //获取数组每个连续有序序列的终止位置，并用队列维护
+  //实际存的是终止位置的下一位置
+  queue<int> que;
+  for(int i=1;i<n;i++){
+    if(arr[i]<arr[i-1])que.push(i);
+  }
+  que.push(n);
+  while(1){
+    //如果队列中只有一个连续有序序列说明已排好序
+    if(que.front()==n)break;
+    //两段序列可用三个端点区分，其中最左端端点l由上一次合并得到，m,r存在队列中
+    int l=0,m,r;
+    while(1){
+      if(l==n)break;
+      m=que.front();
+      que.pop();
+      //需要处理连续序列段数的奇偶问题
+      if(m<n){
+        r=que.front();
+        que.pop();
+        if(flag)mergeArray(arr,l,m,r,temp);
+        else mergeArray(temp,l,m,r,arr);
+        //更新连续序列
+        que.push(r);
+      }else{
+        //奇数情况
+        if(flag)move(arr,l,m,temp);
+        else move(temp,l,m,arr);
+        que.push(m);
+      }
+      l=que.back();
+    }
+    //已全部合并到另一数组中
+    flag=!flag;
+    //if(flag)printArray(arr,n);
+    //else printArray(temp,n);
+  }
+  if(!flag)move(temp,0,n,arr);
+}
+
+int main(){
+  freopen("in.txt","r",stdin);
+  int n;
+  cin>>n;
+  int *arr = new int[n];
+  for(int i=0;i<n;i++)cin>>arr[i];
+  mergeSort(arr,n);
+  printArray(arr,n);
+  delete [] arr;
+  return 0;
+}
---
+++ AlgorithmCourse/Set.cpp
@@ -0,0 +1,53 @@
+//算法课作业
+//求A,B集合的(A-B)+(B-A)
+
+#include <cstdio>
+#include <iostream>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void printArray(int a[],int size_a){
+  for(int i=0;i<size_a;i++)cout<<a[i]<<" ";
+  cout<<endl;
+}
+
+bool bi_search(int x,int arr[],int size_arr){
+  int l=0,r=size_arr;
+  while(l<r){
+    int mid=(r-l)/2+l;
+    if(arr[mid]<x)l=mid+1;
+    else if(arr[mid]==x)return false;
+    else r=mid;
+  }
+  return true;
+}
+
+void solve(int a[],int size_a,int b[],int size_b,int c[],int &size_c){
+  sort(a,a+size_a);
+  sort(b,b+size_b);
+  for(int i=0;i<size_a;i++){
+    if(bi_search(a[i],b,size_b))c[size_c++]=a[i];
+  }
+  for(int i=0;i<size_b;i++){
+    if(bi_search(b[i],a,size_a))c[size_c++]=b[i];
+  }
+}
+
+int main(){
+  freopen("in.txt","r",stdin);
+  int size_a,size_b,size_c=0;
+  cin>>size_a>>size_b;
+  int *a = new int[size_a];
+  int *b = new int[size_b];
+  int *c = new int[size_a+size_b];
+  for(int i=0;i<size_a;i++)cin>>a[i];
+  for(int i=0;i<size_b;i++)cin>>b[i];
+  solve(a,size_a,b,size_b,c,size_c);
+  printArray(c,size_c);
+  delete [] a;
+  delete [] b;
+  delete [] c;
+  return 0;
+}
