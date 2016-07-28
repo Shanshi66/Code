7dc219cf638bff6ecf16d95f0d87512cb96bb313
Rosen <Rosen9212@gmail.com>
2016-07-28 17:50:23 +0800

    easy 11位精度 也是坑

--- LeetCode/326.cpp
+++ LeetCode/326.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        
+        double x = log(n)/log(3);
+        const double eps = 1e-11;
+        //精度1, 0.99
+        if(abs(x - floor(x)) < eps || abs(x - floor(x)) >= 1 - eps) return true;
+        else return false;
     }
 };
