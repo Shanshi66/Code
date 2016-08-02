766e01a48f36195d3efccf88002958cbb441da43
Rosen <Rosen9212@gmail.com>
2016-08-02 14:18:14 +0800

    hiho 矩阵加速

--- Hiho/test.cpp
+++ Hiho/test.cpp
@@ -1,10 +1,26 @@
+#include <iostream>
+#include <map>
+#include <sstream>
 #include <stdio.h>
-#include <string>
-
 
 using namespace std;
 
 int main(){
-
-	return 0;
+	map<string, string> mapper;
+	string str;
+	bool flag = false;
+    while(getline(cin, str)){
+		if(str != ""){
+			if(flag){
+				if(mapper.find(str) == mapper.end()) cout << "eh" << endl;
+				else cout << mapper[str] << endl;
+			}else{
+				string a, b;
+				istringstream str_input(str);
+				str_input >> a >> b;
+				mapper[b] = a;
+			}
+		}else flag = true;
+    }
+    return 0;
 }
