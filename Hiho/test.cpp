008336bf2a35a4e96816c806992bece7634c7d9f
Rosen <Rosen9212@gmail.com>
2016-08-04 09:50:56 +0800

    nim 带解释

--- Hiho/test.cpp
+++ Hiho/test.cpp
@@ -1,26 +1,67 @@
 #include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
 #include <map>
-#include <sstream>
-#include <stdio.h>
 
 using namespace std;
+#define N -999999999
+
+int array[50050],mark[50050],back[50050];
+int n;
+int cmax;
+
+void init(){
+	for(int i = 0; i <= n; i++){
+		mark[i] = back[i] = 0;
+	}
+	cmax = N;
+}
 
 int main(){
-	map<string, string> mapper;
-	string str;
-	bool flag = false;
-    while(getline(cin, str)){
-		if(str != ""){
-			if(flag){
-				if(mapper.find(str) == mapper.end()) cout << "eh" << endl;
-				else cout << mapper[str] << endl;
-			}else{
-				string a, b;
-				istringstream str_input(str);
-				str_input >> a >> b;
-				mapper[b] = a;
+	// freopen("in.txt", "r", stdin);
+	int cases;
+	scanf("%d",&cases);
+	while(cases--)
+	{
+		scanf("%d",&n);
+		init();
+		for(int i=1;i<=n;i++)
+		{
+			scanf("%d",&array[i]);
+			if(i==1){
+				mark[i]=array[i];
 			}
-		}else flag = true;
-    }
-    return 0;
+			else{
+				if(mark[i-1]<0) mark[i]=array[i];
+				else
+					mark[i]=mark[i-1]+array[i];
+			}
+		}
+		for(int i=n;i!=0;i--)
+		{
+			if(i==n){
+				back[i]=array[i];
+			}
+			else {
+				if(back[i+1] < 0) back[i]=array[i];
+				else back[i]=back[i+1]+array[i];
+			}
+		}
+		for(int i=2;i!=n+1;i++){
+			 mark[i]=max(mark[i],mark[i-1]);
+		}
+		for(int i=n-1;i!=0;i--){
+			 back[i]=max(back[i],back[i+1]);
+		}
+		for(int i=1;i<n;i++){
+			cmax=max(mark[i]+back[i+1],cmax);
+		}
+		printf("%d\n",cmax);
+	}
+	return 0;
 }
+
