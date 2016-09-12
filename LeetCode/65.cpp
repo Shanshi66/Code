a72c89023104d2a76060a61c61754fb5f06b1070
Rosen <Rosen9212@gmail.com>
2016-09-12 19:55:37 +0800

    not done

---
+++ LeetCode/65.cpp
@@ -0,0 +1,19 @@
+/*
+题意：
+Validate if a given string is numeric.
+
+Some examples:
+"0" => true
+" 0.1 " => true
+"abc" => false
+"1 a" => false
+"2e10" => true
+
+*/
+
+class Solution {
+public:
+    bool isNumber(string s) {
+        
+    }
+};
