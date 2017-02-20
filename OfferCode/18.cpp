e775bf3593ccd4a99a5bf4ce75d72a4204e127ba
Rosen <Rosen9212@gmail.com>
2017-02-20 09:40:24 +0800

    树的镜像

---
+++ OfferCode/18.cpp
@@ -0,0 +1,24 @@
+/*
+题目：树的镜像
+*/
+
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+*/
+
+class Solution {
+public:
+    void Mirror(TreeNode *pRoot) {
+		if(pRoot == NULL) return;
+        swap(pRoot->left, pRoot->right);
+        Mirror(pRoot->left);
+        Mirror(pRoot->right);
+    }
+};
