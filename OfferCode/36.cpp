ff7916a7ef89eba4c6c5d00360dab17f424cb0c1
Rosen <Rosen9212@gmail.com>
2017-03-02 15:48:59 +0800

    链表的第一个公共结点

--- OfferCode/36.cpp
+++ OfferCode/36.cpp
@@ -3,7 +3,6 @@
 
 */
 
-
 /*
 struct ListNode {
 	int val;
@@ -12,6 +11,9 @@ struct ListNode {
 			val(x), next(NULL) {
 	}
 };*/
+
+
+//解法一，哈希
 class Solution {
 public:
     ListNode* FindFirstCommonNode( ListNode* pHead1, ListNode* pHead2) {
@@ -31,5 +33,49 @@ public:
             p = p->next;
         }
         return res;
+    }
+};
+
+//解法二：因为链表末尾部分一定相同，可以让长的链表先走一部分，等长后在同时走，第一个相同的节点就是答案
+
+/*
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};*/
+class Solution {
+public:
+    int getListLength(ListNode* phead){
+        int len = 0;
+        while(phead != NULL) {
+            len++;
+            phead = phead->next;
+        }
+        return len;
+    }
+    ListNode* FindFirstCommonNode( ListNode* pHead1, ListNode* pHead2) {
+        int len1 = getListLength(pHead1);
+        int len2 = getListLength(pHead2);
+        ListNode* p1 = NULL, *p2 = NULL;
+        if(len1 > len2) {
+            p1 = pHead1;
+            p2 = pHead2;
+        } else {
+            p1 = pHead2;
+            p2 = pHead1;
+        }
+        int k = abs(len1 - len2);
+		while(k--){
+            p1 = p1->next;
+        }
+        while(p1 != NULL && p2 != NULL) {
+            if(p1 == p2) break;
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        return p1;
     }
 };
