68565ecb44445f53dcf07eaddb713eeac5257df1
Rosen <Rosen9212@gmail.com>
2017-05-15 11:19:23 +0800

    链表模拟加法，跟简单的写法

--- LeetCode/2.cpp
+++ LeetCode/2.cpp
@@ -27,35 +27,51 @@ public:
             return l2;
         }
     }
-    void addNumber(ListNode* l1, ListNode* l2){
+    // void addNumber(ListNode* l1, ListNode* l2){
+    //     int jin = 0;
+    //     while(l2 != NULL){
+    //         l1->val += l2->val + jin;
+    //         if(l1->val >= 10) {
+    //             jin = 1;
+    //             l1->val %= 10;
+    //         } else {
+    //             jin = 0;
+    //         }
+    //         if(l2->next != NULL) {
+    //             l1 = l1->next;
+    //             l2 = l2->next;
+    //         } else break;
+    //     }
+    //     while(jin > 0) {
+    //         if(l1->next != NULL) {
+    //             l1->next->val += jin;
+    //             if(l1->next->val >= 10) {
+    //                 l1->next->val %= 10;
+    //                 jin = 1;
+    //             } else {
+    //                 jin = 0;
+    //             }
+    //         } else {
+    //             l1->next = new ListNode(jin);
+    //             jin = 0;
+    //         }
+    //         l1 = l1->next;
+    //     }
+    // }
+    void addNumber(ListNode *l1, ListNode *l2) {
         int jin = 0;
-        while(l2 != NULL){
-            l1->val += l2->val + jin;
+        ListNode *pre = NULL;
+        while(l1 != NULL) {
+            if(l2 != NULL) l1->val += l2->val + jin;
+            else l1->val += jin;
             if(l1->val >= 10) {
+                l1->val -= 10;
                 jin = 1;
-                l1->val %= 10;
-            } else {
-                jin = 0;
-            }
-            if(l2->next != NULL) {
-                l1 = l1->next;
-                l2 = l2->next;
-            } else break;
-        }
-        while(jin > 0) {
-            if(l1->next != NULL) {
-                l1->next->val += jin;
-                if(l1->next->val >= 10) {
-                    l1->next->val %= 10;
-                    jin = 1;
-                } else {
-                    jin = 0;
-                }
-            } else {
-                l1->next = new ListNode(jin);
-                jin = 0;
-            }
+            } else jin = 0;
+            pre = l1;
             l1 = l1->next;
+            if(l2 != NULL) l2 = l2->next;
         }
+        if(jin > 0) pre->next = new ListNode(jin);
     }
 };
